f36a9652b44a8c1afc7e8b4a3bd21a5b261a4f8e
Skyzap <izzat.jehan@gmail.com>
2020-12-21 18:40:45 +0800

    Refactored penis text

--- components/penis/index.js
+++ components/penis/index.js
@@ -1,4 +1,4 @@
 export const penisCommand = (message, args) => {
   const length = Math.floor(Math.random() * 24);
-  message.reply("Your penis is " + length.toString() + " inches");
+  message.reply("your penis is " + length.toString() + " inches");
 };
