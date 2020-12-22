f8413ee3fcee43bb0b44dda1fe265af23d898f84
Pee Aun <peeaun99@gmail.com>
2020-12-22 19:55:00 +0800

    Removed js files

--- components/music/destroy.js
+++
@@ -1,8 +0,0 @@
-export const musicDestroy = async(message, args) => {
-    const voiceChannel = message.member.voice.channel;
-
-    if (!voiceChannel) return message.channel.send('You need to be in the voice channel');
-
-    await voiceChannel.leave();
-    await message.channel.send("I'll be back")
-}
--- components/music/stop.js
+++
@@ -1,9 +0,0 @@
-export const musicStop = async(message, args) => {
-    const voiceChannel = message.member.voice.channel;
-
-    if (!voiceChannel) return message.channel.send('You need to be in the voice channel');
-
-    const videoResult = await ytSearch(query);
-    videoResults.video[0];
-    await message.channel.send("I'm putting a stop to the music");
-}
