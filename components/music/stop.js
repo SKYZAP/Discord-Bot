9dd84fb9fc9de37969e1b7dbcc22071cd359fa15
Pee Aun <peeaun99@gmail.com>
2020-12-22 16:48:36 +0800

    Reverted back to the initial state

--- components/music/stop.js
+++ components/music/stop.js
@@ -1,9 +1,9 @@
-// export const musicStop = async(message, args) => {
-//     const voiceChannel = message.member.voice.channel;
+export const musicStop = async(message, args) => {
+    const voiceChannel = message.member.voice.channel;
 
-//     if (!voiceChannel) return message.channel.send('You need to be in the voice channel');
+    if (!voiceChannel) return message.channel.send('You need to be in the voice channel');
 
-//     const videoResult = await ytSearch(query);
-//     videoResults.video[0];
-//     await message.channel.send("I'm putting a stop to the music");
-// }
+    const videoResult = await ytSearch(query);
+    videoResults.video[0];
+    await message.channel.send("I'm putting a stop to the music");
+}
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import { penisCommand } from "./components/penis/index.js";
 import { pingCommand } from "./components/ping/index.js";
-// import { musicPlay } from "./components/music/play.js";
+import { musicPlay } from "./components/music/play.js";
 // import { musicStop } from "./components/music/stop.js";
 import { musicDestroy } from "./components/music/destroy.js";
 import Discord from "discord.js";
 
 const discordBot = new Discord.Client();
-
-import Player from "discord-player";
-const player = new Player(discordBot);
-discordBot.player = player;
 
 const prefix = "/";
 
@@ -33,19 +29,13 @@ discordBot.on("message", async(message) => {
         let track = await discordBot.player.stop(message.guild.id);
         message.channel.send(`ZAWARUDO`);
     } else if (command === "play") {
-        let track = await discordBot.player.play(message.member.voice.channel, args[0], message.member.user.tag);
-        message.channel.send(`Currently playing ${track.name}! - Requested by ${track.requestedBy}`);
+        musicPlay(message, args);
     } else if (command === "destroy") {
         musicDestroy(message, args);
     } else if (command === "pause") {
         let track = await discordBot.player.pause(message.guild.id);
         message.channel.send("Hold up waita minute")
-    } else if (command === "remove") {
-        let track = await discordBot.player.remove(message.member.voice.channel, args[''], message.member.user.tag);
-        message.channel.send(`Removing requested song`);
-    } else if (command === "resume") {
-        let track = await discordBot.player.resume(message.guild.id);
-        message.channel.send(`Time is resumed`)
+
     } else if (command === "ping") {
         pingCommand(message, args);
     }
