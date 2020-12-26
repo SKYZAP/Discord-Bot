102d23e31801c8624d06f9067379eb22ca45b6cf
Skyzap <izzat.jehan@gmail.com>
2020-12-26 23:52:53 +0800

    Added logging for penis component

--- components/penis/index.ts
+++ components/penis/index.ts
@@ -1,4 +1,17 @@
+import { log } from "../../utils/index";
+
 export const penisCommand = (message, args) => {
   const length = Math.floor(Math.random() * 24);
   message.reply("your penis is " + length.toString() + " inches");
+
+  const channelType = message.channel.name ?? "private message";
+
+  log(
+    "[BerdBot] - " +
+      message.author.username +
+      " used command penis in " +
+      channelType +
+      " channel",
+    "lightblue"
+  );
 };
