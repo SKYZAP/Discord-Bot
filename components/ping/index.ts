777c0f497172c0742d568a88b5ee9e0343b7c68c
Skyzap <izzat.jehan@gmail.com>
2020-12-26 23:52:59 +0800

    Added logging for ping command

--- components/ping/index.ts
+++ components/ping/index.ts
@@ -1,5 +1,18 @@
+import { log } from "../../utils/index";
+
 export const pingCommand = (message, args) => {
   const moment = require("moment");
   let now = moment().millisecond();
   message.reply("Your ping is " + now + "ms");
+
+  const channelType = message.channel.name ?? "private message";
+
+  log(
+    "[BerdBot] - " +
+      message.author.username +
+      " used command ping in " +
+      channelType +
+      " channel",
+    "lightblue"
+  );
 };
