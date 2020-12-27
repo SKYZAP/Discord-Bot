b63632cb3f45e3d90b5dfa4d494430c1b81cc62d
Skyzap <izzat.jehan@gmail.com>
2020-12-27 15:57:52 +0800

    Added express package setup

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 import DiscordBotApp from "./index";
 
 try {
+  const express = require("express");
+  const app = express();
+
+  app.listen(process.env.PORT || 5000);
+
   DiscordBotApp();
 } catch (error) {
   console.log(error);
