5497aa8abad0b57c03e581107b293ae8266ba600
Skyzap <izzat.jehan@gmail.com>
2020-12-27 15:42:49 +0800

    Fixed formatting and styling for penis command

--- components/penis/index.ts
+++ components/penis/index.ts
@@ -1,17 +1,17 @@
 import { getUserFromMention, log } from "../../utils/index";
 
-export const penisCommand = (message, args) => {
+export const penisCommand = (discordBot, message, args) => {
   const length = Math.floor(Math.random() * 24);
 
   if (args[0]) {
-    const user = getUserFromMention(args[0]);
+    const user = getUserFromMention(args[0], discordBot);
     if (!user) return message.reply("User not found");
     message.channel.send(
-      `${user.tag} penis size is ***` + length.toString() + " inches***"
+      `<@${user.id}> penis size is ***` + length.toString() + " inches***"
     );
+  } else {
+    message.reply("your penis size is ***" + length.toString() + " inches***");
   }
-
-  message.reply("your penis size is ***" + length.toString() + " inches***");
 
   const channelType = message.channel.name ?? "private message";
 
