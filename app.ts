3c48214281a51f027188b2d1a15fb283955e0eed
Skyzap <izzat.jehan@gmail.com>
2021-01-01 20:34:07 +0800

    Removed unused code lines

--- app.ts
+++ app.ts
@@ -1,16 +1,6 @@
 import DiscordBotApp from "./index";
-import { log } from "./utils";
-
-const liveCheck = () => {
-  log("[BerdBot] - Funtional", "grey");
-};
 
 try {
-  const express = require("express");
-  const app = express();
-
-  app.listen(process.env.PORT || 5000);
-  setInterval(liveCheck, 50000);
   DiscordBotApp();
 } catch (error) {
   console.log(error);
