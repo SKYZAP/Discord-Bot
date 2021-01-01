739d46e9195bf1e7a1ea4d53add891ce64f0230f
Skyzap <izzat.jehan@gmail.com>
2021-01-01 23:30:05 +0800

    Refactored music functions to have proper error handling

--- components/music/index.ts
+++ components/music/index.ts
@@ -3,9 +3,11 @@ import { log } from "../../utils/index";
 
 export const playMusic = async (discordBot, message, args) => {
   try {
-    const voiceChannel = message.member.voice.channel;
+    const voiceChannel = message.member?.voice.channel;
     if (message.channel.type === "dm" || !voiceChannel) {
-      message.channel.send("You need to be in a voice channel to play songs");
+      message.channel.send(
+        "> [ERROR] You need to be in a voice channel to play songs"
+      );
     } else {
       const search = args.join(" ");
       discordBot.player.play(message, search);
@@ -24,118 +26,213 @@ export const playMusic = async (discordBot, message, args) => {
 };
 
 export const musicDestroy = async (discordBot, message) => {
-  discordBot.player.stop(message);
-  message.channel.send("> [DISCONNECT] I'll be back");
-  log(
-    "[BerdBot] - " +
-      message.author.username +
-      " disconnected the bot from " +
-      message.channel.name +
-      " channel",
-    "lightblue"
-  );
+  try {
+    const voiceChannel = message.member?.voice.channel;
+    if (message.channel.type === "dm" || !voiceChannel) {
+      message.channel.send(
+        "> [ERROR] You need to be in a voice channel to play songs"
+      );
+    } else {
+      discordBot.player.stop(message);
+      message.channel.send("> [DISCONNECT] I'll be back");
+      log(
+        "[BerdBot] - " +
+          message.author.username +
+          " disconnected the bot from " +
+          message.channel.name +
+          " channel",
+        "lightblue"
+      );
+    }
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
 };
 
 export const musicSkip = async (discordBot, message) => {
-  discordBot.player.skip(message);
-  message.channel.send("> [SKIPPED] The current song has been skipped");
-  log(
-    "[BerdBot] - " +
-      message.author.username +
-      " skipped a song from " +
-      message.channel.name +
-      " channel",
-    "lightblue"
-  );
+  try {
+    const voiceChannel = message.member?.voice.channel;
+    if (message.channel.type === "dm" || !voiceChannel) {
+      message.channel.send(
+        "> [ERROR] You need to be in a voice channel to play songs"
+      );
+    } else if (!discordBot.player.isPlaying(message)) {
+      message.channel.send("> [ERROR] No song is being played");
+    } else {
+      discordBot.player.skip(message);
+      message.channel.send("> [SKIPPED] The current song has been skipped");
+      log(
+        "[BerdBot] - " +
+          message.author.username +
+          " skipped a song from " +
+          message.channel.name +
+          " channel",
+        "lightblue"
+      );
+    }
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
 };
 
 export const musicPause = async (discordBot, message) => {
-  discordBot.player.pause(message);
-  message.channel.send("> [PAUSED] The song is now paused");
-  log(
-    "[BerdBot] - " +
-      message.author.username +
-      " paused a song from " +
-      message.channel.name +
-      " channel",
-    "lightblue"
-  );
+  try {
+    const voiceChannel = message.member?.voice.channel;
+    if (message.channel.type === "dm" || !voiceChannel) {
+      message.channel.send(
+        "> [ERROR] You need to be in a voice channel to play songs"
+      );
+    } else if (!discordBot.player.isPlaying(message)) {
+      message.channel.send("> [ERROR] No song is being played");
+    } else {
+      discordBot.player.pause(message);
+      message.channel.send("> [PAUSED] The song is now paused");
+      log(
+        "[BerdBot] - " +
+          message.author.username +
+          " paused a song from " +
+          message.channel.name +
+          " channel",
+        "lightblue"
+      );
+    }
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
 };
 
 export const musicRemove = async (discordBot, message, args) => {
-  const num = parseInt(args[0]);
-  if (num === 0) {
-    musicSkip(discordBot, message);
-  } else {
-    const removedTrack = discordBot.player.remove(message, parseInt(args[0]));
-    message.channel.send(`> [REMOVED] ${removedTrack.title} has been removed`);
+  try {
+    const voiceChannel = message.member?.voice.channel;
+    if (message.channel.type === "dm" || !voiceChannel) {
+      message.channel.send(
+        "> [ERROR] You need to be in a voice channel to play songs"
+      );
+    } else if (!discordBot.player.isPlaying(message)) {
+      message.channel.send("> [ERROR] No song is being played");
+    } else {
+      const num = parseInt(args[0]);
+      if (num === 0) {
+        musicSkip(discordBot, message);
+      } else {
+        const removedTrack = discordBot.player.remove(
+          message,
+          parseInt(args[0])
+        );
+        message.channel.send(
+          `> [REMOVED] ${removedTrack.title} has been removed`
+        );
 
-    log(
-      "[BerdBot] - " +
-        message.author.username +
-        " removed a song from queue in " +
-        message.channel.name +
-        " channel",
-      "lightblue"
-    );
+        log(
+          "[BerdBot] - " +
+            message.author.username +
+            " removed a song from queue in " +
+            message.channel.name +
+            " channel",
+          "lightblue"
+        );
+      }
+    }
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
   }
 };
 
 export const musicResume = async (discordBot, message) => {
-  discordBot.player.resume(message);
-  log(
-    "[BerdBot] - " +
-      message.author.username +
-      " resumed a song from " +
-      message.channel.name +
-      " channel",
-    "lightblue"
-  );
+  try {
+    const voiceChannel = message.member?.voice.channel;
+    if (message.channel.type === "dm" || !voiceChannel) {
+      message.channel.send(
+        "> [ERROR] You need to be in a voice channel to play songs"
+      );
+    } else if (!discordBot.player.isPlaying(message)) {
+      message.channel.send("> [ERROR] No song is being played");
+    } else {
+      discordBot.player.resume(message);
+      message.channel.send("> [RESUMED] The song is now resumed");
+      log(
+        "[BerdBot] - " +
+          message.author.username +
+          " resumed a song from " +
+          message.channel.name +
+          " channel",
+        "lightblue"
+      );
+    }
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
 };
 
 export const musicQueue = async (discordBot, message) => {
-  const queue = await discordBot.player.getQueue(message);
-  let queueMessage: String[] = [];
-
-  if (queue.tracks.length === 0 || queue.tracks === undefined) {
-    message.channel.send("> [QUEUE] There are no upcoming songs");
-  }
-
-  if (!queue) {
-    message.channel.send("> [QUEUE] There is no music queue");
-  }
-
-  queue.tracks.map((q, index) => {
-    if (index === 0) {
-      queueMessage.push(
-        `>>> ======================================================================= \n [${index}]  \t\t${q.title} \n =======================================================================`
+  try {
+    const voiceChannel = message.member?.voice.channel;
+    if (message.channel.type === "dm" || !voiceChannel) {
+      message.channel.send(
+        "> [ERROR] You need to be in a voice channel to play songs"
       );
+    } else if (!discordBot.player.isPlaying(message)) {
+      message.channel.send("> [ERROR] No song is being played");
     } else {
-      queueMessage.push(
-        ` [${index}]  \t\t${q.title} \n =======================================================================`
+      const queue = await discordBot.player.getQueue(message);
+      let queueMessage: String[] = [];
+
+      if (queue.tracks.length === 0 || queue.tracks === undefined) {
+        message.channel.send("> [QUEUE] There are no upcoming songs");
+      }
+
+      if (!queue) {
+        message.channel.send("> [QUEUE] There is no music queue");
+      }
+
+      queue.tracks.map((q, index) => {
+        if (index === 0) {
+          queueMessage.push(
+            `>>> ======================================================================= \n [${index}]  \t\t${q.title} \n =======================================================================`
+          );
+        } else {
+          queueMessage.push(
+            ` [${index}]  \t\t${q.title} \n =======================================================================`
+          );
+        }
+      });
+      message.channel.send(queueMessage);
+      log(
+        "[BerdBot] - " +
+          message.author.username +
+          " viewed the queue in " +
+          message.channel.name +
+          " channel",
+        "lightblue"
       );
     }
-  });
-  message.channel.send(queueMessage);
-  log(
-    "[BerdBot] - " +
-      message.author.username +
-      " viewed the queue in " +
-      message.channel.name +
-      " channel",
-    "lightblue"
-  );
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
 };
 
 export const musicClearQ = async (discordBot, message) => {
-  discordBot.player.clearQueue(message);
-  message.channel.send(`The queue has been cleared`);
-  log(
-    "[BerdBot] - " +
-      message.author.username +
-      " cleared the song queue in " +
-      message.channel.name +
-      " channel",
-    "lightblue"
-  );
+  try {
+    const voiceChannel = message.member?.voice.channel;
+    if (message.channel.type === "dm" || !voiceChannel) {
+      message.channel.send(
+        "> [ERROR] You need to be in a voice channel to play songs"
+      );
+    } else if (!discordBot.player.isPlaying(message)) {
+      message.channel.send("> [ERROR] No song is being played");
+    } else {
+      discordBot.player.clearQueue(message);
+      message.channel.send(`> [CLEAR] The queue has been cleared`);
+      log(
+        "[BerdBot] - " +
+          message.author.username +
+          " cleared the song queue in " +
+          message.channel.name +
+          " channel",
+        "lightblue"
+      );
+    }
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
 };
