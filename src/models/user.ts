4e7b1deb699179009baade60bb2cdbc213ac0445
Skyzap <izzat.jehan@gmail.com>
2021-01-18 02:44:35 +0800

    Added user model

---
+++ src/models/user.ts
@@ -0,0 +1,80 @@
+import { Entity, Column, PrimaryGeneratedColumn, getConnection } from "typeorm";
+import { log } from "../../utils";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  discordId: string;
+
+  @Column()
+  username: string;
+
+  @Column("integer")
+  length: number;
+
+  @Column()
+  playtime: number;
+}
+
+export const addUser = async (message, length, playtime) => {
+  try {
+    const con = getConnection();
+    let user = new User();
+    user.discordId = message.author.id;
+    user.username = message.author.username;
+    user.length = length;
+    user.playtime = playtime;
+
+    let repository = con.getRepository(User);
+    const userExist = await repository.findOne({
+      discordId: message.author.id,
+    });
+
+    if (!userExist) {
+      const saved = await repository.save(user);
+      log("[BerdBot] - User has been saved: " + saved.username, "lightblue");
+    }
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
+};
+
+export const getUser = async (message) => {
+  try {
+    const con = getConnection();
+    let repository = con.getRepository(User);
+    const user = await repository.findOne({
+      discordId: message.author.id,
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    log("[BerdBot] - Found User: " + user.username, "lightblue");
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
+};
+
+export const deleteUser = async (message) => {
+  try {
+    const con = getConnection();
+    let repository = con.getRepository(User);
+    const user = await repository.findOne({
+      discordId: message.author.id,
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    await repository.delete({ id: user.id });
+    log("[BerdBot] - Deleted User: " + user.username, "lightblue");
+  } catch (error) {
+    log("[BerdBot] - " + error.message, "red");
+  }
+};
