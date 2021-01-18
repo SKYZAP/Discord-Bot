ef30230a9525f3f231c3f4754bbf63071c61aabd
Skyzap <izzat.jehan@gmail.com>
2021-01-19 01:34:02 +0800

    Refactored connection options

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import {
   createConnection,
   getConnectionOptions,
 } from "typeorm";
-
+const { parse } = require("pg-connection-string");
 // require("dotenv").config();
 
 export const log = (msg, color) => {
@@ -30,24 +30,6 @@ export const getUserFromMention = (mention, discordBot) => {
   }
 };
 
-// export const options: ConnectionOptions = {
-//   type: "postgres",
-//   host: process.env.DATABASE_URL,
-//   port: parseInt(process.env.DB_PORT),
-//   username: process.env.DB_HOST ?? process.env.DB_USER,
-//   password: process.env.DB_HOST ?? process.env.DB_PASSWORD,
-//   database: process.env.DB_HOST ?? process.env.DB_NAME,
-//   entities: [__dirname + "/../src/models/*.ts"],
-//   synchronize: true,
-//   logging: true,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-//   extra: {
-//     ssl: true,
-//   },
-// };
-
 const getOptions = async () => {
   let connectionOptions: ConnectionOptions;
   connectionOptions = {
@@ -63,7 +45,7 @@ const getOptions = async () => {
     entities: [__dirname + "/../src/models/*.ts"],
   };
   if (process.env.DATABASE_URL) {
-    Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+    Object.assign(connectionOptions, { url: parse(process.env.DATABASE_URL) });
   } else {
     connectionOptions = await getConnectionOptions();
   }
