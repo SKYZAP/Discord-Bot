1e1325e2f6d5cb5dedc56e1b19d49ad9631774fa
Skyzap <izzat.jehan@gmail.com>
2021-01-27 02:22:44 +0800

    Fixed prefix and reminder function

--- index.ts
+++ index.ts
@@ -105,7 +105,7 @@ const DiscordBotApp = () => {
       console.log("reset");
       resetLength(message);
     } else if (command === "remind") {
-      //addReminder(discordBot, message, args);
+      addReminder(discordBot, message, args);
     }
   });
 
