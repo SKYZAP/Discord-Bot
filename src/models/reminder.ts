23a1d1a0e17e9ec4fe5893d95c08b597a6a96d5d
Skyzap <izzat.jehan@gmail.com>
2021-01-27 15:11:46 +0800

    Fixed offset options

--- src/models/reminder.ts
+++ src/models/reminder.ts
@@ -72,7 +72,7 @@ export const addReminder = async (discordBot, message, args) => {
     reminder.message = reminderMsg;
     reminder.time = moment(reminderTime, "DD-MM-YYYY HH:mm")
       .utc(false)
-      .utcOffset(offset)
+      .utcOffset(offset, false)
       .toDate();
     reminder.userId = userExist.id;
     reminder.user = userExist;
