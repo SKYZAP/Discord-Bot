8e9fc6854b4577ff1d42020dffc7582ff1dbfd26
Skyzap <izzat.jehan@gmail.com>
2021-09-21 18:18:14 +0800

    Updated ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,10 @@
 module.exports = {
   type: "postgres",
-  host: "ec2-54-156-73-147.compute-1.amazonaws.com",
-  port: 5432,
-  username: "juulcplyczjkyj",
-  password: "79810764ded614f6ece0f01778314acf075923a46454ca9659d345cfd4d81ada",
-  database: "d2t507ca37r1tp",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   extra: {
     ssl: {
       rejectUnauthorized: false,
