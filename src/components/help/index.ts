01b575ee8541919ae38974af1ac6a668725405a0
Skyzap <izzat.jehan@gmail.com>
2021-09-30 16:59:38 +0800

    Updated help component

--- src/components/help/index.ts
+++ src/components/help/index.ts
@@ -3,34 +3,34 @@ export const helpCommand = (discordBot, message, args) => {
   const helpMessage =
     `**Available commands:**\n` +
     `\n [**Misc:tools:**]` +
-    "\n``/penis``" +
-    `[(i)](${message.url} "Used as /penis or /penis @User"),` +
-    "``/slap``" +
-    `[(i)](${message.url} "Used as /slap or /slap @User"),` +
-    "``/ping``" +
-    `[(i)](${message.url} "Used as /ping")\n` +
+    "\n``!penis``" +
+    `[(i)](${message.url} "Used as ${process.env.PREFIX}penis or ${process.env.PREFIX}penis @User"),` +
+    "``!slap``" +
+    `[(i)](${message.url} "Used as ${process.env.PREFIX}slap or ${process.env.PREFIX}slap @User"),` +
+    "``!ping``" +
+    `[(i)](${message.url} "Used as ${process.env.PREFIX}ping")\n` +
     `\n [**Music:musical_note:**]` +
-    "\n``/play``," +
-    "``/remove``," +
-    "``/queue``," +
-    "``/clearq``," +
-    "``/destroy``," +
-    "``/skip``," +
-    "``/pause``," +
-    "``/resume``\n" +
+    "\n``!play``," +
+    "``!remove``," +
+    "``!queue``," +
+    "``!clearq``," +
+    "``!destroy``," +
+    "``!skip``," +
+    "``!pause``," +
+    "``!resume``\n" +
     `\n [**Filter:microphone2:**]` +
-    "\n``/filter [filtername] [true/false]``" +
+    "\n``!filter [filtername] [true/false]``" +
     `[(i)](${message.url} "Available filters:\nbassboost,\nnightcore,\nvibrato,\n8d,\nvaporwave,\nphaser,\ntremolo,\nreverse,\ntreble,\nnormalizer,\nsurrounding,\npulsator,\nsubboost,\nkaraoke,\nflanger,\ngate,\nhaas,\nmcompand"),` +
-    "``/reset``" +
+    "``!reset``" +
     `[(i)](${message.url} "Used to reset all filters")` +
     "\n\n[**Meme:robot:**]\n" +
-    "``/bonk``," +
-    "``/cmm``," +
-    "``/ohno``," +
-    "``/opinion``," +
-    "``/jail``," +
-    "``/hitler``," +
-    "``/animetiddies``" +
+    "``!bonk``," +
+    "``!cmm``," +
+    "``!ohno``," +
+    "``!opinion``," +
+    "``!jail``," +
+    "``!hitler``," +
+    "``!animetiddies``" +
     `[(i)](${message.url} "Used for big badonkadonks, really huge honkabadukas, absolutely massive badinkadoinkaronies")`;
   const tooltipEmbed = new Discord.MessageEmbed()
     .setColor("#4287f5")
