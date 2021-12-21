c48dfd0f2f17c07fa54ce6261add14273494b141
SKYZAP <izzat.jehan@gmail.com>
2021-12-22 04:01:10 +0800

    Added clearq command

---
+++ src/commands/clearq.ts
@@ -0,0 +1,32 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { player } from "../utils/berdbot-client";
+import { ChannelCheck } from "../utils/music-player";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("clearq")
+    .setDescription("Clears all the songs in the queue"),
+  async execute(interaction) {
+    try {
+      // Check for voice channel
+      ChannelCheck(interaction);
+
+      // Clear function
+      const queue = await player.getQueue(interaction.guildId);
+      if (!queue) {
+        return await interaction.reply({
+          content: "❌ | The queue is not found!",
+          ephemeral: true,
+        });
+      }
+
+      await queue.destroy(true);
+
+      return await interaction.reply({
+        content: `:regional_indicator_x: | **Queue has been cleared**`,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+};
