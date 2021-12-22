b4c194c5e58b5ae573e14e2b283ad14e314fe8c4
SKYZAP <izzat.jehan@gmail.com>
2021-12-23 01:06:46 +0800

    Added paginate utility function

---
+++ src/utils/paginate.ts
@@ -0,0 +1,9 @@
+export const paginate = (arr, size) => {
+  return arr.reduce((acc, val, i) => {
+    let idx = Math.floor(i / size);
+    let page = acc[idx] || (acc[idx] = []);
+    page.push(val);
+
+    return acc;
+  }, []);
+};
