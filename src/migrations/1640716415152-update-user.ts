39df90c83d07c83b111cff900e6c7e123256756f
SKYZAP <izzat.jehan@gmail.com>
2021-12-29 02:34:48 +0800

    Added update user table migration

---
+++ src/migrations/1640716415152-update-user.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class updateUser1640716415152 implements MigrationInterface {
+    name = 'updateUser1640716415152'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "updatedAt"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "createdAt"`);
+    }
+
+}
